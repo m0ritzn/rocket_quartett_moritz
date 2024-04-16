5515ababe7d110690c8dcc5657fd06dc68ec99a6
m0ritzn <moritz.nussbaumer@hfg.design>
2024-04-16 17:06:43 +0200

    Farben und Konturen angepasst

--- card_simple/script/main.js
+++ card_simple/script/main.js
@@ -2,12 +2,39 @@ $(document).ready(function () {
     const wrapper = $('#wrapper');
     const imagePaths = ["images/falcon_heavy.jpeg", "images/saturn_v.jpeg", "images/ariane_5.jpeg", "images/falcon_heavy.jpeg", "images/electron.jpeg", "images/vega.jpeg", "images/falcon_1.webp", "images/pegasus.jpeg", "images/voyager_1.jpeg", "images/dart.jpeg", "images/new_horizons.webp", "images/juno.jpeg", "images/hubble.jpeg", "images/jwst.jpeg", "images/kepler.jpeg", "images/gps.jpeg", "images/curiosity.jpeg", "images/perseverance.webp", "images/zhurong.png", "images/opportunity.jpeg", "images/insight.jpeg", "images/change_4.png", "images/venera_10.jpeg", "images/hayabusa_2.webp", "images/iss.jpeg", "images/tiangong.jpeg", "images/mir.jpeg", "images/skylab.jpeg", "images/crew_dragon.webp", "images/apollo.jpeg", "images/space_shuttle.webp", "images/soyuz.webp"];
 
+    function getColorByGroupLetter(groupLetter) {
+        switch (groupLetter) {
+            case 'A':
+                return '#023059';
+            case 'B':
+                return '#046FCD';
+            case 'C':
+                return '#430E17';
+            case 'D':
+                return '#290147';
+            case 'E':
+                return '#8C331F';
+            case 'F':
+                return '#D99559';
+            case 'G':
+                return '#132999';
+            case 'H':
+                return '#494F50';
+            
+            default:
+                return '#000000'; // Standardfarbe Schwarz
+        }
+    }
+
     rocketData.forEach((rocket, index) => {
+        const groupColor = getColorByGroupLetter(rocket.group_letter); 
+
+
         const card =
             $(`<div class="card">
         <div class="card-content">
-        <div class="card-number">${rocket.group_letter}${rocket.number}</div>
-        <div class="card-title">
+        <div class="card-number" style="background-color: ${groupColor};">${rocket.group_letter}${rocket.number}</div>
+        <div class="card-title" style="background-color: ${groupColor};">
         <div class="card-name">${rocket.name}</div>
         <div class="card-group">${rocket.group}</div>
         </div>
