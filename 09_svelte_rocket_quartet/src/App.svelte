d76f18e3cad420243a4597f2ad35d22aed01da57
m0ritzn <moritz.nussbaumer@hfg.design>
2024-04-23 23:44:47 +0200

    vorhandenes Raketen Quartett grob in svelte übertragen

---
+++ 09_svelte_rocket_quartet/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import rocketData from "./data/data.js";
+  import Card from "./Card.svelte";
+  import Header from "./Header.svelte";
+</script>
+
+<Header />
+<main>
+  {#each rocketData as rocket, index}
+    <Card {rocket} {index} />
+  {/each}
+</main>
---
+++ 09_svelte_rocket_quartet/src/Header.svelte
@@ -0,0 +1,35 @@
+<header>
+    <h1>Rocket Quartet</h1>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+
+<style>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 20px;
+        background-color: #222;
+        color: #fff;
+    }
+
+    nav ul {
+        display: flex;
+        list-style: none;
+    }
+
+    nav ul li {
+        margin-right: 20px;
+    }
+
+    nav ul li a {
+        color: #fff;
+        text-decoration: none;
+    }
+</style>
