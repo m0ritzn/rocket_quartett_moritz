6bbd0422d75481855218631d3fa19c48c11fd7d1
m0ritzn <moritz.nussbaumer@hfg.design>
2024-04-24 14:26:30 +0200

    erste Schritt für Tabellen Ansicht

---
+++ 10_svelte_rocket_table/src/components/Rocketrow.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let rocket;
+</script>
+
+<tr>
+    <td>{rocket.group_letter}{rocket.number}</td>
+    <td>{rocket.name}</td>
+    <td>{rocket.max_speed}</td>
+    <td>{rocket.year_built}</td>
+</tr>
+
+<style>
+</style>
