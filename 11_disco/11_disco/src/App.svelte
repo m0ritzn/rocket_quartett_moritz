60e761e4000071baed892f53280a861ebe78648e
m0ritzn <moritz.nussbaumer@hfg.design>
2024-05-08 09:12:20 +0200

    dico erstellt

---
+++ 11_disco/11_disco/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Floor_01 from "./components/Floor_01.svelte";
+</script>
+
+<div id="wrapper">
+  <main>
+    <Floor_01 />
+  </main>
+</div>
+
+<style>
+</style>
---
+++ 11_disco/11_disco/src/components/Floor_01.svelte
@@ -0,0 +1,17 @@
+<script>
+console.log("test");
+
+</script>
+
+<div class="floor">
+
+
+</div>
+
+<style>
+    floor {
+        width: 250px;
+        height: 600px;
+        background-color: #000;
+    }
+</style>
