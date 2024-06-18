bedc532ad1e93a838d44c80a48bccd4e61a2db99
m0ritzn <moritz.nussbaumer@hfg.design>
2024-06-18 23:12:17 +0200

    CORS Problem gefixt und navbar erstelle
    CORS Pronlematik via Proxyserver umgangen

--- astro_app/src/App.svelte
+++ astro_app/src/App.svelte
@@ -1,8 +1,55 @@
 <script>
-  import SatelliteData from "./ObjectSatellite.svelte";
+  import Navbar from "./components/Navbar.svelte";
+
+  import { onMount } from "svelte";
+
+  let data;
+
+  async function fetchData() {
+    try {
+      const response = await fetch(
+        // `/api/rest/v1/satellite/above/48/9/0/10/0/&apiKey=${import.meta.env.VITE_API_KEY}`,
+        `/api/rest/v1/satellite/above/48/9/0/10/0/&apiKey=H5W4P7-C4NVAN-VVAXWF-59LC`,
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      data = await response.json();
+      console.log(data); // Print the data to the console
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  onMount(() => {
+    fetchData();
+  });
 </script>
 
-<main></main>
+<!-- {#if data}
+  <ul>
+    {#each data.above as satellite}
+      <li>{satellite.satname}</li>
+    {/each}
+  </ul>
+{:else}
+  <p>Loading...</p>
+{/if} -->
+
+<main>
+  <Navbar />
+  <!-- <section id="visualisierung">
+    <h2>Visualisierung</h2>
+
+  </section>
+  <section id="tabelle">
+    <h2>Tabelle</h2>
+
+  </section> -->
+</main>
 
 <style>
+  main {
+    padding: 2em;
+  }
 </style>
---
+++ astro_app/src/components/Navbar.svelte
@@ -0,0 +1,59 @@
+<script>
+</script>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Navigation Bar</title>
+    <link rel="stylesheet" href="styles.css" />
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="logo">What's above you?</div>
+            <div class="nav-links">
+                <a class="nav-link" href="#visualisierung">Visualisierung</a>
+                <a class="nav-link" href="#tabelle">Tabelle</a>
+            </div>
+        </nav>
+    </header>
+</body>
+
+<style>
+    :global(body) {
+        margin: 0;
+        font-family: Arial, sans-serif;
+        background-color: #1c1c2d;
+        color: white;
+    }
+
+    .navbar {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1em 2em;
+        background-color: #1c1c2d;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+
+    .logo {
+        font-size: 1.5em;
+        font-weight: bold;
+    }
+
+    .nav-links {
+        display: flex;
+    }
+
+    .nav-link {
+        margin-left: 2em;
+        color: white;
+        text-decoration: none;
+        font-size: 1em;
+        transition: color 0.3s;
+    }
+
+    .nav-link:hover {
+        color: #ffcc00;
+    }
+</style>
